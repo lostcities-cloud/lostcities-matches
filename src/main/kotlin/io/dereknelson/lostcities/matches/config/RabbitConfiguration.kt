45be0437c50d09e79464f0e1e189ddf65ffd9c22
Derek <contact@dereknelson.io>
2022-02-28 13:38:05 -0500

    Updating gradle.properties

--- src/main/kotlin/io/dereknelson/lostcities/matches/config/RabbitConfiguration.kt
+++ src/main/kotlin/io/dereknelson/lostcities/matches/config/RabbitConfiguration.kt
@@ -1,5 +1,7 @@
 package io.dereknelson.lostcities.matches.config
 
+import io.dereknelson.lostcities.matches.service.MatchEventService.Companion.END_GAME_EVENT
+import io.dereknelson.lostcities.matches.service.MatchEventService.Companion.TURN_CHANGE_EVENT
 import io.dereknelson.lostcities.matches.service.MatchService.Companion.CREATE_GAME_QUEUE
 import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.Queue
@@ -23,13 +25,27 @@ class RabbitConfiguration(
     @PostConstruct
     fun initialize() {
         try {
+            declare(
+                Queue(CREATE_GAME_QUEUE, true, false, false),
+                Binding(CREATE_GAME_QUEUE, Binding.DestinationType.QUEUE, exchange, "*", null)
+            )
 
-            val queue = Queue(CREATE_GAME_QUEUE, true, false, false)
-            val binding = Binding(CREATE_GAME_QUEUE, Binding.DestinationType.QUEUE, exchange, "*", null)
-            admin.declareQueue(queue)
-            admin.declareBinding(binding)
+            declare(
+                Queue(TURN_CHANGE_EVENT, true, false, false),
+                Binding(TURN_CHANGE_EVENT, Binding.DestinationType.QUEUE, exchange, "*", null)
+            )
+
+            declare(
+                Queue(END_GAME_EVENT, true, false, false),
+                Binding(END_GAME_EVENT, Binding.DestinationType.QUEUE, exchange, "*", null)
+            )
         } catch (e: Exception) {
             println("Alright.")
         }
+    }
+
+    private fun declare(queue: Queue, binding: Binding) {
+        admin.declareQueue(queue)
+        admin.declareBinding(binding)
     }
 }
