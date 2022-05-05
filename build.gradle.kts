187f181656381182ac8d56e3bc0176ae9aaa6474
Derek <contact@dereknelson.io>
2022-05-04 23:00:50 -0400

    Removing undertow to default tomcat.

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,10 +66,7 @@ dependencies {
 
 	implementation("org.apache.commons:commons-lang3:3.12.0")
 
-	implementation("org.springframework.boot:spring-boot-starter-web") {
-		exclude(module="spring-boot-starter-tomcat")
-	}
-
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
