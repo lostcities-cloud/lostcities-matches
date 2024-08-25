7ada9b1334261bfbe56613d913f063806baf584c
nelson54 <me@dereknelson.io>
2024-08-25 12:36:15 -0400

    Using consul in dev.

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 
+    implementation("org.springframework.cloud:spring-cloud-dependencies")
+    implementation("org.springframework.cloud:spring-cloud-starter-consul-discovery")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-hppc")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
