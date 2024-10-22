40f26373918eb80eb4696dfea7673441cbf1aea9
Derek Nelson <me@derekenelson.io>
2024-10-22 01:52:36 -0400

    ktlint

--- src/main/kotlin/io/dereknelson/lostcities/matches/config/SecurityConfiguration.kt
+++ src/main/kotlin/io/dereknelson/lostcities/matches/config/SecurityConfiguration.kt
@@ -15,10 +15,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.DefaultSecurityFilterChain
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
